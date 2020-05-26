8bd50afbf82ff427ca7959f081684e131e771423
Venko Trajkovski <infidel756@gmail.com>
2020-05-26 22:57:36 +0200

    First Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
